932e68f5af28c79686a8a7c3dc53446c7fb9fe9b
fuzoh <info@bastiennicoud.ch>
2025-01-15 14:21:23 +0100

    chore: breeze installation

--- _ide_helper.php
+++ _ide_helper.php
@@ -22385,6 +22385,75 @@ namespace Illuminate\Http {
             return \Illuminate\Http\Request::hasValidRelativeSignatureWhileIgnoring($ignoreQuery);
         }
 
+        /**
+         * 
+         *
+         * @see \Inertia\ServiceProvider::registerRequestMacro()
+         * @static 
+         */
+        public static function inertia()
+        {
+            return \Illuminate\Http\Request::inertia();
+        }
+
+            }
+    }
+
+namespace Illuminate\Routing {
+    /**
+     * 
+     *
+     * @mixin \Illuminate\Routing\RouteRegistrar
+     */
+    class Router {
+        /**
+         * 
+         *
+         * @see \Inertia\ServiceProvider::registerRouterMacro()
+         * @param mixed $uri
+         * @param mixed $component
+         * @param mixed $props
+         * @static 
+         */
+        public static function inertia($uri, $component, $props = [])
+        {
+            return \Illuminate\Routing\Router::inertia($uri, $component, $props);
+        }
+
+            }
+    }
+
+namespace Illuminate\Testing {
+    /**
+     * 
+     *
+     * @template TResponse of \Symfony\Component\HttpFoundation\Response
+     * @mixin \Illuminate\Http\Response
+     */
+    class TestResponse {
+        /**
+         * 
+         *
+         * @see \Inertia\Testing\TestResponseMacros::assertInertia()
+         * @param \Closure|null $callback
+         * @static 
+         */
+        public static function assertInertia($callback = null)
+        {
+            return \Illuminate\Testing\TestResponse::assertInertia($callback);
+        }
+
+        /**
+         * 
+         *
+         * @see \Inertia\Testing\TestResponseMacros::inertiaPage()
+         * @static 
+         */
+        public static function inertiaPage()
+        {
+            return \Illuminate\Testing\TestResponse::inertiaPage();
+        }
+
             }
     }
 
