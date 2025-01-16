565e1392849813b99e62bfae43059e5bd3862295
fuzoh <info@bastiennicoud.ch>
2025-01-16 12:19:48 +0100

    chore: sanctum

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[ApiResource]
+#[ApiResource(
+
+)]
 class Todo extends Model
 {
     /** @use HasFactory<\Database\Factories\TodoFactory> */
