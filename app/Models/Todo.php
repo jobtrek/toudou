64a4b4b162bfd8d0de77429740ae07fa80acbcd1
Bastien Nicoud <info@bastiennicoud.ch>
2025-01-16 12:23:02 +0100

    feat: api setup (#3)
    * chore: breeze installation
    
    * feat: todo entity and migration
    
    * chore: install api platform
    
    * feat: api endpoint for todo model
    
    * feat: todo seeder
    
    * chore: sanctum
    
    * style: format

---
+++ app/Models/Todo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use ApiPlatform\Metadata\ApiResource;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#[ApiResource(
+
+)]
+class Todo extends Model
+{
+    /** @use HasFactory<\Database\Factories\TodoFactory> */
+    use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'due_date' => 'datetime',
+        ];
+    }
+}
---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'content' => fake()->text(60),
+            'description' => fake()->text(150),
+            'done' => fake()->boolean(),
+            'due_date' => fake()->date(),
+        ];
+    }
+}
